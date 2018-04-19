ddb2538125e244bedca9c90c0e00c3d8ad66cfe2
Maciej Buszka <maciej@buszka.eu>
2018-04-19 10:48:43 +0200

    ex 4, 5 dedup

--- apache-tinkerpop-gremlin-console-3.3.2/list-3/ex-4.groovy
+++ apache-tinkerpop-gremlin-console-3.3.2/list-3/ex-4.groovy
@@ -1,3 +1,3 @@
 g.V().has('code', 'WRO').
-  repeat(out()).times(4).emit().
+  repeat(out()).times(4).emit().dedup().
   has('country', 'UK').path().by('code')
--- apache-tinkerpop-gremlin-console-3.3.2/list-3/ex-5.groovy
+++ apache-tinkerpop-gremlin-console-3.3.2/list-3/ex-5.groovy
@@ -1,3 +1,3 @@
 g.V().has('code', 'WRO').
-  repeat(out().has('country', 'UK')).times(4).emit().
+  repeat(out().has('country', 'UK')).times(4).emit().dedup().
   path().by('code')
